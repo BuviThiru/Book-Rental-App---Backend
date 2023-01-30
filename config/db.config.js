0afad3dac55f15912262514dc63ebda8673d4e69
Buvaneshwari <thirubuvana@gmail.com>
2023-01-30 12:23:47 +0530

    six

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,13 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: mavima14, // update the db password here
+    PASSWORD: process.env.PASSWORD, // update the db password here
     DB: "bookrentalapplicationdatabase", //add database name here
     dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
-      idle: 10000,
-      
+      idle: 10000,      
     }
   };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,8 +1,8 @@
 const {getAllBooks,addBook,delRentReturn,booksRentedByaUser} = require("../controllers/books.contr")
 const {isAuthenticated,isAdmin} = require("../middleware/authentication")
 const routes =(app)=>{ 
-    app.get('/books/list',isAuthenticated, getAllBooks)
-    app.post('/book/create',isAuthenticated,isAdmin, addBook)
+    app.get('/books/list',isAuthenticated, getAllBooks)  //gets all the book list
+    app.post('/book/create',isAuthenticated,isAdmin, addBook) //add the book list
     app.post('/book',isAuthenticated, isAdmin, delRentReturn)
     app.get('/rented/:userId',isAuthenticated, booksRentedByaUser)
 }
